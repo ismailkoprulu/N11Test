4fe3eba7786b0f5b47657825a2d6f795086612a2
ismail Koprulu <ismailkoprulu@hotmail.com>
2017-09-21 22:13:55 +0300

    commit

--- n11TestOdevi/src/test/java/FacebookLogin.java
+++ n11TestOdevi/src/test/java/FacebookLogin.java
@@ -1,10 +1,16 @@
 import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 
 public class FacebookLogin {
 
     //testcase1
     @Test
     public void shouldLoginWithFacebookAccount(){
+
+        System.setProperty("webdriver.chrome.driver", "/Users/user/Downloads/chromedriver");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.n11.com/");
 
     }
 }
